5c9b2b6206ca55597ff27c00f1feb9d68e511c1a
CHO YUN SIK <postop09@naver.com>
2022-02-24 21:17:28 +0900

    :hammer: 로그인 API 연동

--- src/JavaScript/login.js
+++ src/JavaScript/login.js
@@ -30,19 +30,19 @@ async function login() {
   const pw = main.querySelector('#inp_customerPw');
   const url = 'http://13.209.150.154:8000';
   const loginData = {
-		"username": id.value,
-		"password": pw.value,
-		"login_type": "BUYER" // BUYER : 일반 구매자, SELLER : 판매자
+		'username': id.value,
+		'password': pw.value,
+		'login_type': 'BUYER' // BUYER : 일반 구매자, SELLER : 판매자
   }
   const res = await fetch(url+'/accounts/login/', {
     method : 'POST',
     headers: {
-      "Content-type" : "application/json"
+      "Content-Type" : "application/json"
     },
     body: JSON.stringify(loginData)
   })
-  console.log(res);
+  const json = await res.json();
+  console.log(json);
 }
-// const btnLogin = main.querySelector('.btn_customerLogin');
-// btnLogin.addEventListener('click', login);
-login()
+const btnLogin = main.querySelector('.btn_customerLogin');
+btnLogin.addEventListener('click', login);
